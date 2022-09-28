c7fdb720a6839dc6f7e342bf2552b0521ca93127
Meocintosh <kimdh7776@naver.com>
2022-09-28 20:00:37 +0900

    수정

--- src/codingTest/Kakao0928.java
+++ src/codingTest/Kakao0928.java
@@ -7,9 +7,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class Kakao0928 {
-	public static String today = "2022.05.19";
-	public static String[] terms = {"A 6", "B 12", "C 3"};
-	public static String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
+//	public static String today = "2022.05.19";
+//	public static String[] terms = {"A 6", "B 12", "C 3"};
+//	public static String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
+	public static String today = "2020.01.01";
+	public static String[] terms = {"Z 3", "D 5"};
+	public static String[] privacies = {"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.20 Z"};
 	public static void main(String[] args) {
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		ArrayList<Integer> list = new ArrayList<>();
@@ -39,8 +42,9 @@ public class Kakao0928 {
 			int day = Integer.parseInt(yearArr[2]); // 약관 시간날짜 일
 			// 우선 해당 개월수만큼 늘려줘야하는데 연도가 바뀌는 작업을 해줘야함
 			if(month + gigan > 12) {
-				year += 1;
-				month = month + gigan - 12;
+				// 근데 2년, 3년 이렇게 돌수도 있으니까 12로 나눠주고 나온 몫만큼 연도를 더해준다
+				year += (month+gigan)/12;
+				month = month + gigan - 12*((month+gigan)/12);
 			} else {
 				month = month + gigan;
 			}
@@ -58,15 +62,15 @@ public class Kakao0928 {
 			}
 			// 여기까지 데드라인을 구했다. year, month, day에 해당 약관의 데드라인이 들어가있다.
 			if(todayYear > year) {
-				list.add(i);
+				list.add(i+1);
 			}
 			else if(todayYear == year) {
 				if(todayMonth > month) {
-					list.add(i);
+					list.add(i+1);
 				}
 				else if(todayMonth == month) {
 					if(todayDay > day) {
-						list.add(i);
+						list.add(i+1);
 					}
 				}
 			}
